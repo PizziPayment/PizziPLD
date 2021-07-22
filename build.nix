a1b3738ed06eb3a2d34743166512130c3d359152
Lilian VERLHAC <lilian.verlhac@outlook.com>
2021-07-22 23:37:10 +0200

    Add build.nix with build environment

---
+++ build.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/b58ada326aa612ea1e2fb9a53d550999e94f1985.tar.gz") {}
+}:
+let
+  packages = python-packages: with python-packages; [
+    pyyaml
+  ];
+  python-packages = pkgs.python3.withPackages packages;
+in
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    tectonic
+    python-packages
+  ];
+}
