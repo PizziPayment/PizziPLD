313c00648070f6272ebd3c0db0ec680955baa490
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-11-27 22:50:36 +0000

    Add backend update 27/11/2022

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/b58ada326aa612ea1e2fb9a53d550999e94f1985.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/b58ada326aa612ea1e2fb9a53d550999e94f1985.tar.gz") { }
 }:
 let
   packages = python-packages: with python-packages; [
